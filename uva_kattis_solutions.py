4e3c5fcdd70f0ae5146cd492f94db60762fb11c5
Anderson Mahosy <andersonmahosi@gmail.com>
2025-03-17 19:04:10 +0300

    gray code

--- uva_kattis_solutions.py
+++ uva_kattis_solutions.py
@@ -5,24 +5,44 @@ import sys
 def main():
     #modulo
     #print(modulo(15,4))
-    print(lsb(1))
+    #print(bin(39),bin(turnOnConsec0(39)))
+    greyCode()
 
 #######tasks with bit manipulation
 
-#Obtain the remainder (modulo) of S when it is divided by N (N is a power of 2)
 def modulo(dvd, dvs):
+    #Obtain the remainder (modulo) of S when it is divided by N (N is a power of 2)
     return ((dvd) & (dvs-1))
 
-#Determine if S is a power of 2.
 def isPower2(n):
+    #Determine if S is a power of 2.
     return (not(n & (n - 1)))
     #return (n & -n) == n
 
-def lsb(x):
-    n = 0
-    while (x | (1 << n)) != x: 
-        n += 1
-    return n
+
+def turnOffConsec1(n):
+    # Turn o, the last consecutive run of ones in S
+    return (n & (n+1))
+
+
+def turnOnConsec0(n):
+    # Turn o, the last consecutive run of ones in S
+    return (n | (n-1))
+
+
+def greyCode():
+    #UVa 11173
+    """
+    pattern 1 : active the first nonzero (from the left) bit of the number
+    looks like xor
+    """
+    inputs = iter(sys.stdin.readlines())
+    n = int(next(inputs))
+    while n > 0 :
+        _, kth = (next(inputs)).split(" ")
+        kth = int(kth)
+        print(kth^(kth>>1))
+        n -= 1
 
 if __name__ == "__main__":
     main()
